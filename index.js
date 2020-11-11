862a7d7971479c38574654a15d17f03bdb662620
justinchrislee <sonyjlee5@gmail.com>
2020-11-10 23:25:39 -0800

    added local passport strategy and keys for configuration

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+));
 
 app.listen(5000);
