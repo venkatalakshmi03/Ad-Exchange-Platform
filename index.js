052a49b4cfe7cc7115c5f70b453b1916bf3ea600
justinchrislee <sonyjlee5@gmail.com>
2020-11-11 14:40:12 -0800

    implemented connection to local mysql database

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
-require('./services/passport');
+const mysql = require('mysql');
+const keys = require('./config/keys');
+require('./services/passport'); // include passport authentication strategy
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: keys.mysqlUser,
+    password: keys.mysqlPassword,
+    database: keys.mysqlDatabase
+});
+
+// connect to mySQL database
+connection.connect(function(err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Database connection successful!");
+});
+
+require('./routes/authRoutes')(app); // authentication routes
 
 app.get('/random', (req, res) => {
-    res.send({ tenet: "no friends at dusk" });
+    res.send({ tenet: "we live in a twilight zone, no friends at dusk" });
 });
 
 
